0907d71f787c32b14ec5f45957bb1c859fb45c24
liontalk@163.com <liontalk@163.com>
2020-01-20 10:01:42 +0800

    数据提交策略

--- mongodb_demo/src/main/java/cn/liontalk/mongodb_demo/MongodbDemoApplication.java
+++ mongodb_demo/src/main/java/cn/liontalk/mongodb_demo/MongodbDemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MongodbDemoApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(MongodbDemoApplication.class, args);
     }
